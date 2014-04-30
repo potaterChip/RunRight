7f26e0e6da768b878c43dd7cb3cf764753b67b27
potaterChip <ericgrobin@gmail.com>
2014-04-29 21:36:15 -0500

    First commit
    From chap 33 base.

---
+++ src/main/java/com/potaterchip/runright/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.potaterchip.runright;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by Eric on 4/1/2014.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+    protected abstract Fragment createFragment();
+}
